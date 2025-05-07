b7be1e1ea365e46246352f122fe0685c0d8a6614
JohnHYepes <jhym91@gmail.com>
2025-05-06 21:36:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const express = require('express');
 const multer = require('multer');
 const crypto = require('crypto');
 const cors = require('cors');
+
 const app = express();
 const upload = multer();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.post('/decrypt', upload.single('file'), async (req, res) => {
-  console.log("BODY:", req.body); // <--- agrega esto
-  console.log("FILE SIZE:", req.file?.buffer?.length || 0);
-  console.log("mediaKey:", req.body.mediaKey);
-
   try {
     const encBuffer = req.file.buffer;
-    const mediaKeyB64 = req.body.media_key;
+    const mediaKeyB64 = req.body.mediaKey;
+
+    if (!mediaKeyB64) {
+      return res.status(400).json({ error: 'Missing mediaKey' });
+    }
+
     const mediaKey = Buffer.from(mediaKeyB64, 'base64');
     const iv = Buffer.alloc(16, 0);
-    const salt = Buffer.from("WhatsApp Voice Keys");
+    const salt = Buffer.from('WhatsApp Voice Keys');
+
     const cipherKey = crypto.createHmac('sha256', mediaKey).update(salt).digest().slice(0, 32);
     const decipher = crypto.createDecipheriv('aes-256-cbc', cipherKey, iv);
     const decrypted = Buffer.concat([decipher.update(encBuffer), decipher.final()]);
